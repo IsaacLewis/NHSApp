6fad58a0d12371cf31cab3d0986edfc50f1afb96
Isaac Lewis <isaac@isaac-dell-laptop>
2011-10-09 11:03:50 +0100

    got touch button working

--- gen/com/svc2uk/R.java
+++ gen/com/svc2uk/R.java
@@ -21,6 +21,10 @@ public final class R {
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
+    public static final class id {
+        public static final int ListView_menu=0x7f060001;
+        public static final int yes_button=0x7f060000;
+    }
     public static final class layout {
         public static final int emergency=0x7f030000;
         public static final int main=0x7f030001;
--- src/com/svc2uk/Hello.java
+++ src/com/svc2uk/Hello.java
@@ -1,7 +1,15 @@
 package com.svc2uk;
 
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Hello extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +17,18 @@ public class Hello extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // String s = findStringById(R.string.yes);
+        //ListView lv = (ListView) findViewById(R.id.ListView_menu);
+        View yes = findViewById(R.id.yes_button);
+        Log.d("sv",(yes == null) + " " + yes);
+        yes.setOnClickListener(new AdapterView.OnClickListener() {
+
+		@Override
+		public void onClick(View view) {
+			startActivity(new Intent(Hello.this,EmergencyNumberInfo.class));
+			
+		}
+	    });
     }
 }
